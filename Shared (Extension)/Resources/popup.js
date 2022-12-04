954295d959a66bb1929cac36c52a20110330b8b5
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2022-12-04 18:25:44 +0000

    turn checkboxes off when we turn on the thing

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -52,6 +52,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
                         chrome.tabs.sendMessage(tabs[0].id, { method: "change", element: some_element, status: false });
                       });
+                    
+                    document.getElementById(some_element + "Toggle").checked = false;
                 });
                 
                 // save state of the toggle
