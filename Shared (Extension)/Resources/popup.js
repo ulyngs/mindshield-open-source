164635e48eac5c105ffa5f8ff6fbec5aa8008eed
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2023-03-23 13:54:48 +0100

    revert to changing visibility to avoid misaligned player size

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -2,6 +2,121 @@
 // https://developer.chrome.com/docs/extensions/mv3/messaging/
 
 document.addEventListener('DOMContentLoaded', function() {
+    // set friction checkbox
+    browser.storage.sync.get("addFriction", function(result) {
+      var frictionToggle = document.getElementById("frictionToggle");
+      var frictionCustomisation = document.querySelectorAll(".friction-customisation");
+      
+      frictionToggle.checked = result.addFriction;
+      
+        if (result.addFriction == undefined || !frictionToggle.checked) {
+            // don't show the customisation options if we haven't checked the box
+            for (var i = 0; i < frictionCustomisation.length; i++) {
+                frictionCustomisation[i].style.display = "none";
+                }
+        } else {
+            for (var i = 0; i < frictionCustomisation.length; i++) {
+                frictionCustomisation[i].style.display = "block";
+                }
+        }
+        
+      var popupContainer = document.getElementById("popup-content");
+      var messageContainer = document.getElementById("delay-content");
+            
+        
+        browser.storage.sync.get("waitText").then(function(result) {
+            var waitText = result.waitText;
+            var messageBox = document.getElementById("delay-message");
+
+            if (waitText != null) {
+                // set the placeholder text
+                document.getElementById("waitText").value = result.waitText;
+                messageBox.innerText = result.waitText;
+            } else {
+                document.getElementById("waitText").value = "What's your intention?";
+                messageBox.innerText = "What is your intention?";
+            }
+        });
+            
+      if(result["addFriction"]) {
+          popupContainer.style.display = "none";
+          messageContainer.style.display = "block";
+          
+          // Wait for 1 second before removing the 'show' class
+            setTimeout(function() {
+              messageContainer.classList.add("show");
+            }, 100);
+                
+        browser.storage.sync.get("waitTime").then(function(result) {
+          var waitTime = result.waitTime;
+          var countdownBox = document.getElementById("delay-time");
+          var countdown = 0;
+                
+          if (waitTime != null) {
+              // set the placeholder text
+              document.getElementById("waitTime").value = waitTime;
+              
+              // Use the wait time value for the count down
+              countdownBox.innerText = waitTime;
+              countdown = waitTime;
+            
+          } else {
+            countdownBox.innerText = "10";
+            document.getElementById("waitTime").value = "10";
+            countdown = 10;
+          }
+                
+          // Update countdown timer every second
+          var timerId = setInterval(function() {
+            countdown--;
+            if (countdown >= 0) {
+              countdownBox.innerText = countdown;
+            } else {
+              // Hide the message box and stop the timer
+              messageContainer.style.display = "none";
+              
+              popupContainer.style.display = "block";
+              clearInterval(timerId);
+            }
+          }, 1000);
+        });
+      } else {
+        messageContainer.style.display = "none";
+        messageContainer.classList.remove("show");
+        popupContainer.style.display = "block";
+          
+      }
+    });
+    
+    var frictionToggle = document.getElementById("frictionToggle");
+    var frictionCustomisation = document.querySelectorAll(".friction-customisation");
+    
+    // Add event listener to detect changes in the checkbox status
+    frictionToggle.addEventListener('change', function() {
+        // If the checkbox is checked, show the delay time input
+        if (frictionToggle.checked) {
+            for (var i = 0; i < frictionCustomisation.length; i++) {
+                frictionCustomisation[i].style.display = "block";
+              }
+        } else {
+            // Otherwise, hide it
+            for (var i = 0; i < frictionCustomisation.length; i++) {
+                frictionCustomisation[i].style.display = "none";
+              }
+        }
+        
+        // store the setting
+        browser.storage.sync.set({ "addFriction": document.getElementById("frictionToggle").checked });
+    });
+    
+    // store wait customisation immediately
+    document.getElementById("waitTime").addEventListener('input', function(){
+        browser.storage.sync.set({ "waitTime": parseInt(document.getElementById("waitTime").value) });
+    });
+    document.getElementById("waitText").addEventListener('input', function(){
+        browser.storage.sync.set({ "waitText": document.getElementById("waitText").value });
+    });
+    
     const platformsWeTarget = [ "youtube", "facebook", "twitter", "instagram", "linkedin", "google" ];
     const elementsThatCanBeHidden = [ "youtubeRecVids",
                                       "youtubeThumbnails",
