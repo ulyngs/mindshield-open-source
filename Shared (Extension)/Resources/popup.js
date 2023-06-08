0307fe169d5e2821fbfd9cea55a60c669f56b926
Ulrik Lyngs <ulrik.lyngs@gmail.com>
2023-06-08 11:05:46 +0200

    add search prediction blocking

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -179,6 +179,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
     const platformsWeTarget = [ "youtube", "facebook", "twitter", "instagram", "linkedin", "whatsapp", "google" ];
     const elementsThatCanBeHidden = [ "youtubeSearch",
+                                      "youtubeSearchPredict",
                                       "youtubeRecVids",
                                       "youtubeThumbnails",
                                       "youtubeProfileImg",
