d242910dad57e6780222f6c3145eb69a49f6a35c
Tiago Costa <tiago@redd-project.org>
2025-09-25 10:11:24 +0100

    fix ig suggested follow selector

--- Shared (Extension)/Resources/selectors.js
+++ Shared (Extension)/Resources/selectors.js
@@ -87,8 +87,8 @@ const cssSelectors = {
     instagramExploreCssOff: 'a[href="/explore/"] { display: none !important; }',
     instagramReelsCssOn: 'a[href="/reels/"] { display: flex !important; }',
     instagramReelsCssOff: 'a[href="/reels/"] { display: none !important; }',
-    instagramSuggestionsCssOn: 'div.x78zum5.xdt5ytf.xdj266r.x11i5rnm.xod5an3.x169t7cy.x1j7kr1c.xvbhtw8:has(a[href="/explore/people/"]) { display: block !important; }',
-    instagramSuggestionsCssOff: 'div.x78zum5.xdt5ytf.xdj266r.x11i5rnm.xod5an3.x169t7cy.x1j7kr1c.xvbhtw8:has(a[href="/explore/people/"]) { display: none !important; }',
+    instagramSuggestionsCssOn: 'div.html-div.x14z9mp.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.xqui205.x1e56ztr.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1:has(a[href="/explore/people/"]) { display: block !important; }',
+    instagramSuggestionsCssOff: 'div.html-div.x14z9mp.x1lziwak.xexx8yu.xyri2b.x18d9i69.x1c1uobl.x9f619.xjbqb8w.x78zum5.x15mokao.x1ga7v0g.x16uus16.xbiv7yw.xqui205.x1e56ztr.x1uhb9sk.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.xdt5ytf.xqjyukv.x1qjc9v5.x1oa3qoh.x1nhvcw1:has(a[href="/explore/people/"]) { display: none !important; }',
     instagramCommentsCssOn: 'div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1xmf6yo:has(a[href*="/comments/"]) {display: block !important;}',
     instagramCommentsCssOff: 'div.x9f619.xjbqb8w.x78zum5.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1xmf6yo:has(a[href*="/comments/"]) {display: none !important;}',
 
